158a714629922f951574b3068370dcb19fef6f7b
Danny <barakadan421@gmail.com>
2023-01-26 01:58:53 +0300

    Set up allow for primary app origins/resource

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,4 +3,10 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         origins 'localhost:3000'
         resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
+
+    allow do
+        # this will be the link of the app that will be using the api eg, origins can be "https://blog-react-app.com"
+        origins 'localhost:3001'
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    end
 end
